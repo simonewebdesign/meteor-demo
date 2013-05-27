d159076c862ddc717da8f26360b520e204fec7a1
Simone <hello@simonewebdesign.it>
2013-05-27 05:50:55 -0400

    Remove method

--- demo.js
+++ demo.js
@@ -26,41 +26,50 @@ if (Meteor.isClient) {
     return "Hello, World!";
   };
 
-  Template.maintemplate.events({
-    'click .insert' : function () {
-      // template data, if any, is available in 'this'
-      console.log("insert");
-      Messages.insert({text: "Hello, World!!!", time: new Date()});
-    },
-    'click .remove' : function () {
-      // template data, if any, is available in 'this'
-      console.log("remove");
-    }
-  });
-
   Template.maintemplate.created = function() {
     console.log("template created");
-  }
-
-  Template.maintemplate.messages = function() {
-    return Messages.find({}, {sort: {time: 1}});
   }
 
   // -------------------------------------
 
   Template.messages_list.title = function() {
-    return "Listing messages!";
+    return "Listing messages";
   }
+
   Template.messages_list.messages = function() {
-    return Messages.find({}, {sort: {time: 1}});
+    return Messages.find({}, {sort: {time: -1}});
   }
+
+
+  Template.maintemplate.events({
+
+    // Add a new message
+    'click .insert' : function () {
+      // template data, if any, is available in 'this'
+      console.log("insert");
+      Messages.insert({text: "FOOBAR", time: new Date()});
+    },
+
+    // Delete all messages
+    'click .remove' : function () {
+      // template data, if any, is available in 'this'
+      console.log("remove");
+
+      // You can use this code on server-side only, for security reasons.
+      // This just won't work here.
+      Messages.remove({}, function() {
+        console.log("Everything was removed.");
+      });
+    }
+  });
 
 }
 
-/*
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    console.log("Server startato. Questo messaggio apparirà sul terminale.");
   });
 }
-*/
+
